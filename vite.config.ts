b9e48e19d9edcc8f32563e5d68fb0641ded533f7
xiaoxiao <1343057333@qq.com>
2026-06-24 23:32:55 +0800

    deploy from docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   base: "/knowledge-graph-garden/",
+  build: {
+    outDir: "docs",
+  },
   plugins: [vue()],
   resolve: {
     alias: {
