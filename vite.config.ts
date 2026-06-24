4ca4e1d5abc0d22ae8097e9b5daedff92607f15a
xiaoxiao <1343057333@qq.com>
2026-06-24 23:09:31 +0800

    second

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/knowledge-graph-garden/",
   plugins: [vue()],
   resolve: {
     alias: {
